338598939d5847fa7512caf8bfee6743fd36bf79
Kevin Cantwell <kevin.cantwell@gmail.com>
2014-05-01 20:51:58 -0400

    works sorta

--- content_script.js
+++ content_script.js
@@ -4,9 +4,18 @@ function fbgif() {
   var wrappers = document.querySelectorAll(".userContentWrapper");
   for (var i = 0; wrappers[i]; i++) {
     var wrapper = wrappers[i];
-    var links = wrapper.querySelectorAll(".userContent a");
+    var links = wrapper.querySelectorAll("a");
     for (var j = 0; links[j]; j++) {
       var link = links[j];
+
+      if( document.createEvent ) {
+          var evObj = document.createEvent('MouseEvents');
+          evObj.initEvent( 'mouseover', true, false );
+          link.dispatchEvent(evObj);
+      } else if( document.createEventObject ) {
+          link.fireEvent('onmouseover');
+      }
+
       var src = link.getAttribute('href').match(/.*\.gif/i);
       if (src) {
         var imgs = link.querySelectorAll("img");
